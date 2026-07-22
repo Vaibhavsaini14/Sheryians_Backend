74f5a76478fc77e4a590291df555b8098f75d0f7
Vaibhav Saini <sainivaibhav1409@gmail.com>
2026-07-22 18:16:47 +0530

    "Lecture 3 : NPM Features"

---
+++ 03_NPM_Basics_to_Adv/script.js
@@ -0,0 +1,18 @@
+// npm ----> node package manager
+
+//Modules -->  already installed in core node js
+//Package -->  Jo hum npm se download karte h
+
+// for installation --> npm install or (i) packagekaName
+
+// for version --> npm i <packagename>@5.0.6 (or any version)
+
+// dependencies --> packages aur unki dep.
+
+//devdependencies ---> jo sirf devlopment mein kaam ayenge pr jb app deploy hojayega then we dont use this
+
+// Scripts --->
+// Jo scripts already installed ya stored h nodejs system mein unke liye hume run likhna nhi pdta 
+
+// But when we write manual scripts like (npm chacha) then no command present...for these scripts we have to add run in front of them
+// run ka matlb ---> Scripts m jaake dekho usse
