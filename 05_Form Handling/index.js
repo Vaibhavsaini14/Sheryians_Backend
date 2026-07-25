7d03febc51787a2c8a10452813f5c0eda7b1b967
Vaibhav Saini <sainivaibhav1409@gmail.com>
2026-07-25 22:27:01 +0530

    "Lecture 5: Form Handling , Session & Cookies

---
+++ 05_Form Handling/index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+
+const app = express()
+
+//Backend process ko handle krta hai & also handle data coming from any library , framework etc.
+
+//Session & Cookie ----->>
+
+//jb bhi app frontend se koi request bhejte ho server pe system does not recognize you always thats why ek string attach kri jaati h req ke sath jiski help se system verifies know ki aap kon ho if not reached or lost the server log out 
+
+app.use(express.json());  // these both are middlewares
+app.use(express.urlencoded({extended : true}));
+
+// Ye dono plain text se jo user se aaya jo trnasmission ke time blob(not readable data ) m converted hai usko vapis se readable bnane m help krte haii
+
+// json format to normal  ke liye 1st wala
+// for ejs and other forms 2nd wala 
