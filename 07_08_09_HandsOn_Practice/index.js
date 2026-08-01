814784be41689957d1b3cb8d7f484b39ba943543
Vaibhav Saini <sainivaibhav1409@gmail.com>
2026-08-01 17:11:46 +0530

    Cretae tasks made routes

---
+++ 07_08_09_HandsOn_Practice/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine' , 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+const path = require('path')
+app.use(express.static(path.join(__dirname , 'public')))
+
+const fs = require('fs');
+app.get("/" , function(req , res){
+    fs.readdir(`./files` , function(err , files){
+        res.render("index" , {files: files});
+    })
+    
+})
+
+app.post("/create" , function(req , res){
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details , function(err){
+        res.redirect("/");
+    })
+})
+
+app.get("/file/:filename" , function(req , res){
+   fs.readFile(`./files/${req.params.filename}`, "utf-8" , function(err , filedata){
+    res.render('show' , {filename: req.params.filename , filedata: filedata})
+   })
+})
+
+app.get("/edit/:filename" , function(req , res){
+res.render('edit' , {filename: req.params.filename});
+})
+
+app.post("/edit" , function(req , res){
+   fs.rename(`./files/${req.body.previous}`, `./files/${req.body.newname}`, function(err){
+    res.redirect("/")
+   })
+})
+
+
+app.listen(3000);
