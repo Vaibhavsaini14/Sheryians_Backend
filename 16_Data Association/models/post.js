48aa7c94356d0129fbe58596de11bb55989f17ca
Vaibhav Saini <sainivaibhav1409@gmail.com>
2026-08-14 01:41:50 +0530

    "Lecture 16: Data Association with MongoDB

---
+++ 16_Data Association/models/post.js
@@ -0,0 +1,15 @@
+const { default: mongoose } = require('mongoose');
+const monngoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+    postdata : String ,
+    user : {
+        type: mongoose.Schema.Types.ObjectId ,
+        ref: 'user'
+    } ,
+    date : {
+        type : Date ,
+        default : Date.now
+    }
+})
+module.exports = mongoose.model('post' , postSchema);
